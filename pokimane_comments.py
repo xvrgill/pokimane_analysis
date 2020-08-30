b729330b5c655b0a32d9eba3e156e5febd868a9b
Xavier Gill <xaviercgill@gmail.com>
2020-08-30 02:54:36 -0400

    fixed comment script formatting & began comment analysis

--- pokimane_comments.py
+++ pokimane_comments.py
@@ -13,14 +13,14 @@ class YTcomments:
         video_dictionary = self._create_video_id_object(channel_data)
         video_data = channel_data['UChXKjLEzAB1K7EZQey7Fm1Q']['video_data']
         for video in video_data:
-            video_dictionary.update({video:[]})
+            video_dictionary.update({video: dict()})
         return video_dictionary
 
     def get_all_comments(self):
         video_dictionary = self.read_channel_data()
         for video_id in tqdm(video_dictionary):
             comments = self._get_comments(video_id)
-            video_dictionary[video_id].append(comments)
+            video_dictionary[video_id].update(comments)
         self.video_dictionary = video_dictionary
         return video_dictionary
 
@@ -34,32 +34,40 @@ class YTcomments:
         while (npt is not None):
             next_url = url +'&pageToken=' + npt
             next_comments, npt = self._get_comments_per_page(next_url)
-            comments.append(next_comments)
+            comments.update(next_comments)
         return comments   
 
     def _get_comments_per_page(self, url):
         json_url = requests.get(url)
         data = json.loads(json_url.text)
-        comments = []
+        comments = dict()
         if 'items' not in data:
             return comments, None
         item_data = data['items']
         nextPageToken = data.get('nextPageToken', None)
         for item in item_data:
-            collection = dict()
+            # TODO: shouldn't need this dictionary... test and delete if necessary
+            # collection = dict()
+            # Get thread id for each thread to use as unique identifier when updating dictionary
+            if item['kind'] == 'youtube#commentThread':
+                thread_id = item['id']
+            # Grab the snippet for use in update method
             snippet = item['snippet']
+            # Grab replies for use in update method
+            # If there are none, set replies to none
             try:
                 replies = item['replies']
             except:
                 replies = None
-
+            # Create new structure for our data
             fused_data = {
-                'snippet': snippet,
-                'replies': replies
+                thread_id: {
+                    'snippet': snippet,
+                    'replies': replies
+                }
             }
-
-            collection.update(fused_data)
-            comments.append(collection)
+            # collection.update(fused_data)
+            comments.update(fused_data)
         return comments, nextPageToken
 
     def dump(self):
